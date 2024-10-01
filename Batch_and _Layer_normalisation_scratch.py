33ce6f27db58bcf4c6d26541250b00f1b5c3672a
Farisa Fathima <148028476+farisafathima@users.noreply.github.com>
2024-10-01 07:49:01 +0530

    Batch_and _Layer_normalisation_scratch.py

--- Batch_and _Layer_normalisation_scratch.py
+++ Batch_and _Layer_normalisation_scratch.py
@@ -1,54 +1,47 @@
 # Batch Normalization from Scratch
 import numpy as np
 
+#technique that normalizes input of each layer so that they have zero mean and unit variance--speed up training, better convergence
 
-def batch_norm(X, gamma, beta, eps=1e-5):
+def batch_norm(x, gamma, beta, eps=1e-5):
+    #gamma -scale parameter
+    #beta - shift parameter
 
-    # Calculate the mean and variance across the batch
-    mean = np.mean(X, axis=0)  
-    variance = np.var(X, axis=0)  
+    mean = np.mean(x,axis=0)
+    variance = np.var(x,axis=0)
+    #variance measures how much values differ from mean
 
-    #  Normalize the batch
-    X_normalized = (X - mean) / np.sqrt(variance + eps)
+    x_normalized = (x-mean)/ np.sqrt(variance+eps)
 
-    # Scale and shift using gamma and beta
-    normalized_X = gamma * X_normalized + beta
+    #scale and shift
+    normalised_x = gamma * x_normalized + beta
+    return normalised_x
 
-    return normalized_X
-
-
-
-X = np.random.randn(4, 3)  
-# print(X)
-gamma = np.ones(3)  
-beta = np.zeros(3)  
-output = batch_norm(X, gamma, beta)
-print("Batch Normalization Output:")
-print(output)
+x = np.random.randn(4,3)
+beta = np.zeros(3)
+gamma = np.ones(3)
+output = batch_norm(x,gamma,beta)
+print("Batch normalization output\n", output)
 
 # Layer Normalization from Scratch
 
 
-def layer_norm(X, gamma, beta, eps=1e-5):
-
-    # Calculate mean and variance for each individual input
-    mean = np.mean(X, axis=1, keepdims=True)  # Mean for each sample
-    variance = np.var(X, axis=1, keepdims=True)  # Variance for each sample
-
-    #Normalize each input
-    X_normalized = (X - mean) / np.sqrt(variance + eps)
-
-    # Scale and shift using gamma and beta
-    normalized_X = gamma * X_normalized + beta
-
-    return normalized_X
+import numpy as np
 
 
+#technique that normalizes across the features of each individual sample, rather than across a batch
+#commonly used for rnn
+def layer_normalization(x,gamma,beta , eps=1e-5):
+    mean = np.mean(x,axis=1, keepdims=True)
+    #keepdims - ensures that the dimensions are retained
+    variance = np.var(x, axis=1, keepdims=True)
+    x_normalized = (x-mean)/ np.sqrt(variance+eps)
+    normalized_x = gamma * x_normalized + beta
+    return normalized_x
 
-X = np.random.randn(4, 3)  
-gamma = np.ones((1, 3))  
-beta = np.zeros((1, 3)) 
-output = layer_norm(X, gamma, beta)
-print("\nLayer Normalization Output:")
-print(output)
+x = np.random.randn(4,3)
+gamma = np.ones((1,3))
+beta = np.zeros((1,3))
+output = layer_normalization(x,gamma,beta)
+print("Layer normalization output\n", output)
 
